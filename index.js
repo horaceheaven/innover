845866c5d063e51572345758442175298ddb4a09
horaceheaven <hheaven@medullan.com>
2014-11-12 20:15:00 -0400

    Using envionment variable for app port

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var express = require('express');
+
 var app = express();
+
+app.set('port', (process.env.PORT || 5000))
+
 app.use('/', express.static(__dirname + '/public'));
-app.listen(5000, function() { console.log('listening')});
+
+app.listen(app.get('port'), function() { 
+  console.log('listening')
+});
